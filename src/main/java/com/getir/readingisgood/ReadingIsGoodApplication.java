f7c70be3eaac1694a5fe0ee771ef86f1196c3458
Alvaro Ferrera <applewhiz@outlook.es>
2022-01-09 12:45:08 +0100

    Add Swagger

--- src/main/java/com/getir/readingisgood/ReadingIsGoodApplication.java
+++ src/main/java/com/getir/readingisgood/ReadingIsGoodApplication.java
@@ -3,7 +3,11 @@ package com.getir.readingisgood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "GetIr ReadingIsGood API", version = "1.0", description = "GetIr Java case study solution by Alvaro Ferrera"))
 public class ReadingIsGoodApplication {
 
 	public static void main(String[] args) {
